696fe6b6228eb34d0035776cf2430ff6ce084abf
Ben Holland Young <bbne45@icloud.com>
2016-12-29 15:27:15 +0000

    Used templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!!"
+    user = {'nickname': 'Ben'} #fake username
+    posts = [
+        {
+            'author': {'nickname': 'John', 'kudos': 34},
+            'body': 'Beautiful day in Portland!!'
+        },
+        {
+            'author': {'nickname': 'Susan', 'kudos': 55},
+            'body': 'Avengers Movie was great'
+            }
+        ]
+
+    return render_template("index.html",
+                          title='Home',
+                          user=user,
+                          posts=posts)
+
+
+
+
+
